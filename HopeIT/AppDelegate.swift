f4c13a5091586b4338526609b50e0a76c18df0a7
Piotr Olejnik <p.olejnik@dook.pl>
2018-02-12 00:41:03 +0100

    Refactor vol 1

--- HopeIT/AppDelegate.swift
+++ HopeIT/AppDelegate.swift
@@ -13,37 +13,32 @@ import FirebaseInstanceID
 import FirebaseMessaging
 import BRYXBanner
 
+    /*
+    Push notification handling needs heavy reimplementation (eg. including authorization stuff)
+    */
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     var window: UIWindow?
-    var paymentAmount: Int?
-    var openMessages = false
+    
+    private var paymentAmount: Int?
+    private var openMessages = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        UITabBar.appearance().tintColor = UIColor.defaultBlue()
-        UITabBar.appearance().shadowImage = UIImage()
-        
-        UINavigationBar.appearance().shadowImage = UIImage()
-        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        UINavigationBar.appearance().backgroundColor = UIColor.clear
-        UINavigationBar.appearance().isTranslucent = true
-        UINavigationBar.appearance().tintColor = UIColor.white
-
+        applyInitialAppearance()
         
         UNUserNotificationCenter.current().delegate = self
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
             completionHandler: {_, _ in })
-        // For iOS 10 data message (sent via FCM
+
         Messaging.messaging().delegate = self
-        
         application.registerForRemoteNotifications()
         FirebaseApp.configure()
-        print(launchOptions?[UIApplicationLaunchOptionsKey.remoteNotification])
         if (launchOptions != nil), let remoteNotification = launchOptions?[UIApplicationLaunchOptionsKey.remoteNotification] as? [AnyHashable : Any] {
-            if let type = remoteNotification["type"] as? String {
+            if let _ = remoteNotification["type"] as? String {
                 openMessages = true
             }
         }
@@ -51,19 +46,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return true
     }
     
-    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-        // Convert to pretty-print JSON, just to show the message for testing
-        guard let data =
-            try? JSONSerialization.data(withJSONObject: remoteMessage.appData, options: .prettyPrinted),
-            let prettyPrinted = String(data: data, encoding: .utf8) else {
-                return
-        }
-        print("Received direct channel message:\n\(prettyPrinted)")
-    }
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) { }
     
-    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
-        print("Firebase registration token: \(fcmToken)")
-    }
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) { }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
@@ -72,7 +57,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Swift.Void) {
         let userInfo = notification.request.content.userInfo
         var amount: Int? = nil
-        print((userInfo["aps"] as? [String: AnyObject])?["alert"])
         if let a = (userInfo["aps"] as? [String: AnyObject])?["alert"] as? String, let int = Int(a) {
             amount = int
         }
@@ -82,15 +66,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Swift.Void) {
         let userInfo = response.notification.request.content.userInfo
         var amount: Int? = nil
-        print((userInfo["aps"] as? [String: AnyObject])?["alert"])
+        
         if let a = (userInfo["aps"] as? [String: AnyObject])?["alert"] as? String, let int = Int(a) {
             amount = int
         }
         action(on: userInfo["type"] as! String, amount: amount)
     }
 
-    func action(on type: String, amount: Int?) {
+    // MARK: Private
+    
+    private func applyInitialAppearance() {
+        UITabBar.appearance().tintColor = UIColor.defaultBlue()
+        UITabBar.appearance().shadowImage = UIImage()
         
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().backgroundColor = UIColor.clear
+        UINavigationBar.appearance().isTranslucent = true
+        UINavigationBar.appearance().tintColor = UIColor.white
+
+    }
+    
+    private func action(on type: String, amount: Int?) {
         var content = "Otrzymano nową wiadomość"
         if type == "" {
             content = "Gratuluję właśnie zrealizowałeś założony cel!"
@@ -111,34 +108,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         banner.show(duration: 2.0)
         
         banner.didTapBlock = {
-            //Redirect to feed
             NotificationCenter.default.post(name: Notification.Name("goToMessages"), object: nil)
         }
-        
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
